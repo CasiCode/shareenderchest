e24b6206ebaf4103a3d2d872fe82aee03648c48f
Casi <73135100+CasiCode@users.noreply.github.com>
2023-10-05 12:50:01 +0400

    Update ShareEnderChest.java
    uncommented blockEntity logic for the chest, commented the opening sound logic

--- src/main/java/me/glitch/aitecraft/shareenderchest/ShareEnderChest.java
+++ src/main/java/me/glitch/aitecraft/shareenderchest/ShareEnderChest.java
@@ -108,11 +108,11 @@ public class ShareEnderChest implements ModInitializer, ServerStopping, ServerSt
 				if (player.isSneaking() && !player.isSpectator()) {
 					if (world.isClient()) return ActionResult.SUCCESS;
 					openSharedEnderChest(player);
-					world.playSound(null, hitResult.getBlockPos(), SoundEvents.BLOCK_ENDER_CHEST_OPEN, SoundCategory.BLOCKS, 1f, 1f);
+					//world.playSound(null, hitResult.getBlockPos(), SoundEvents.BLOCK_ENDER_CHEST_OPEN, SoundCategory.BLOCKS, 1f, 1f); // commented
 					return ActionResult.SUCCESS;
 
-					//EnderChestBlockEntity blockEntity = (EnderChestBlockEntity) world.getBlockEntity(hitResult.getBlockPos());
-					//sharedInventory.setBlockEntity(player, blockEntity);
+					EnderChestBlockEntity blockEntity = (EnderChestBlockEntity) world.getBlockEntity(hitResult.getBlockPos()); //uncommented
+					sharedInventory.setBlockEntity(player, blockEntity); //uncommented
 				}
 			}
 			return ActionResult.PASS;
